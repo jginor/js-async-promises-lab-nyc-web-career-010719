a7dd4529a6353db4b2b98220ca9a7e55ded366a4
github username <github email address>
2019-02-23 17:05:07 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,48 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question){
+  let questionContainer = document.querySelector(".question-container")
+  questionContainer.innerHTML = question.questionText
+}
+
+function askQuestionThen(time){
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise(function(resolve){
+    setTimeout(function(){
+      resolve(question)
+    }, time)
+  })
+}
+
+function removeQuestion(question){
+  return new Promise(function(){
+  let container = document.querySelector('.question-container')
+  container.innerHTML = ''
+  toggleTrueAndFalseButtons()
+})
+}
+
+function trueAndFalseButtons(){
+   return btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach(function(btn){
+    btn.classList.toggle("hide")
+  })
+}
+
+function displayQuestionOnClick(){
+  let btn = document.querySelector('a')
+  return btn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
+
+function askQuestionThenRemoveQuestion(time){
+  return askQuestionThen(time).then(removeQuestion)
+}
